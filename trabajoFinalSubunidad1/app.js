ddc104de303e5c72989adc4de1ea6b3ab9955465
Luisa Castaño <luisa.castano40@eia.edu.co>
2024-04-17 16:04:51 -0500

    [update] servidor 17/04/2024

---
+++ trabajoFinalSubunidad1/app.js
@@ -0,0 +1,39 @@
+// 1. IMPORTAMOS dependencias y/o modulos Y CONFIGURAMOS EXPRESS COMO SERVIDOR
+//importamos dependencias según sistema de módulos ES6
+import express from 'express';
+// Importamos path para usar ciertas utilidades a la hora de trabajar con rutas
+import path from 'path';
+
+//configuramos express como servidor
+const app = express();
+// puerto que vamos a usar para escuchar las solicitudes
+const port = 3000; 
+
+
+// 2. ESTABLECEMOS LAS RUTAS PARA NUESTRO FRONT
+
+// obtenemos la ruta absoluta de nuestra carpeta public
+const publicPath = path.join(process.cwd(), 'public');
+// configuramos middleware
+// middleware para usar los archivos estáticos dentro de public
+app.use(express.static(publicPath));
+// middleware para cuando esperas recibir datos en formato JSON
+app.use(express.json());
+
+
+
+
+// Ruta para index.html
+// manejo de solicitud en una aplicación Node.js
+app.get('/', (req, res) => {
+    // req -> request - solicitud del cliente
+    // res -> res - respuesta por parte del servidor
+    // sendFile -> enviamos una ruta como respuesta
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+
+// 3. INICIALIZAMOS EL SERVIDOR
+app.listen(port, () => {
+    console.log(`El servidor está escuchando en http://localhost:${port}`);
+  });
